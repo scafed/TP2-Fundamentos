00f468a23b631c8fe7c309b621b882b127ba4e39
scafed <fnscalise@gmail.com>
2026-06-27 19:20:32 -0300

    Agrego las PRE y POST condiciones | Agrego emojis y mejor visual por pantalla | Cambio de lejibilidad de algunas funciones

--- batalla_naval.c
+++ batalla_naval.c
@@ -37,6 +37,11 @@
 #define HUNDIDO 'H'
 #define VACIO '-'
 #define BARCO 'B'
+#define EMOJI_BARCO "\U0001F6A2"
+#define EMOJI_AGUA "\U0001f7e6"
+#define EMOJI_TOCADO "\U0001F4A5"
+#define EMOJI_HUNDIDO "\u2620\uFE0F "
+#define EMOJI_VACIO "\u2B1B"
 #define ERROR_AL_RESERVAR_MEMORIA -1
 #define ERROR_ARGUMENTOS -2
 #define ERROR_ARCHIVO_APERTURA -3
@@ -58,27 +63,27 @@ typedef struct estado_barco {
 } estado_barco_t;
 
 /*
- * PRE: 
- * POST: 
+ * PRE: -
+ * POST: Devuelve true si la cantidad de argumentos que se reciben es igual a CANT_ARGUMENTOS, false en caso contrario.
  */
 bool validar_argumentos(int argc) {
     return (argc == CANT_ARGUMENTOS);
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: La coordenada recibida por parámetro debe estar cargada con fila y columna.
+ * POST: Devuelve true si la coordenada está dentro de los indices del tablero (entre las coordenadas (FILA_MIN_TABLERO, COLUMNA_MIN_TABLERO) y (FILA_MAX_TABLERO, COLUMNA_MAX_TABLERO)), false en caso contrario.
  */
 bool es_coordenada_valida_tablero(coordenada_t coordenada) {
-    return !((coordenada.fila < FILA_MIN_TABLERO) || 
-             (coordenada.fila > FILA_MAX_TABLERO) || 
-             (coordenada.columna < COLUMNA_MIN_TABLERO) || 
-             (coordenada.columna > COLUMNA_MAX_TABLERO));
+    return ((coordenada.fila >= FILA_MIN_TABLERO) &&
+            (coordenada.fila <= FILA_MAX_TABLERO) &&
+            (coordenada.columna >= COLUMNA_MIN_TABLERO) &&
+            (coordenada.columna <= COLUMNA_MAX_TABLERO));
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: La coordenada recibida por parámetro debe estar cargada con fila y columna.
+ * POST: Devuelve true si la coordenada está dentro del tablero (entre las coordenadas (FILA_MIN_USUARIO, COLUMNA_MIN_USUARIO) y (FILA_MAX_USUARIO, COLUMNA_MAX_USUARIO)), false en caso contrario.
  */
 bool es_coordenada_valida_usuario(coordenada_t coordenada) {
     return ((coordenada.fila >= FILA_MIN_USUARIO) && 
@@ -88,24 +93,24 @@ bool es_coordenada_valida_usuario(coordenada_t coordenada) {
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: -
+ * POST: Devuelve true si la dirección es igual a DIRECCION_NORTE o DIRECCION SUR o DIRECCION_ESTE o DIRECCION_OESTE, false en caso contrario.
  */
 bool es_direccion_valida(char direccion) {
-    return !((direccion != DIRECCION_NORTE) && (direccion != DIRECCION_SUR) && (direccion != DIRECCION_ESTE) && (direccion != DIRECCION_OESTE));
+    return ((direccion == DIRECCION_NORTE) || (direccion == DIRECCION_SUR) || (direccion == DIRECCION_ESTE) || (direccion == DIRECCION_OESTE));
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: El largo del barco debe estar cargado previamente.
+ * POST: Devuelve true si el largo del barco está dentro de los límites LARGO_BARCO_MINIMO y LARGO_BARCO_MAXIMO, false en caso contrario.
  */
 bool es_largo_valido(int largo) {
-    return !((largo < LARGO_BARCO_MINIMO) || (largo > LARGO_BARCO_MAXIMO));
+    return ((largo >= LARGO_BARCO_MINIMO) && (largo <= LARGO_BARCO_MAXIMO));
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: La coordenada, direccion y largo del barco deben ser válidos.
+ * POST: Crea al barco del jugador cargando sus coordenadas en función de la direccion, largo, y coordenada inicial. Devuelve el estado de crear el barco: en caso de que se salga del límite del tablero o haya un error de memoria, devuelve el número de error, sino devuelve OK.
  */
 int crear_barco(barco_t *barco, coordenada_t coordenada, char direccion, int largo) {
     int i = 0;
@@ -154,8 +159,8 @@ int crear_barco(barco_t *barco, coordenada_t coordenada, char direccion, int lar
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: Debe haber por lo menos un barco cargado.
+ * POST: Devuelve true si existe superposcion de coordenadas entre barcos, false en caso contrario.
  */
 bool hay_superposicion_de_barcos(barco_t barcos_jugador[CANT_BARCOS], int cant_barcos_leidos) {
     int ultimo_barco = cant_barcos_leidos - 1;
@@ -189,8 +194,8 @@ bool hay_superposicion_de_barcos(barco_t barcos_jugador[CANT_BARCOS], int cant_b
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: Deben estar los barcos creados previamente.
+ * POST: Devuelve true si la composicion de la flota (CANT_BARCOS_LARGO_2, CANT_BARCOS_LARGO_3, CANT_BARCOS_LARGO_4, CANT_BARCOS_LARGO_5) coincide, false en caso contrario.
  */
 bool composicion_flota_valida(barco_t barcos_jugador[CANT_BARCOS]) {
     int cant_largo_2 = 0;
@@ -225,8 +230,8 @@ bool composicion_flota_valida(barco_t barcos_jugador[CANT_BARCOS]) {
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: Debe haber por lo menos un barco creado previamente con crear_barco().
+ * POST: Elimina los barcos creados, liberando la memoria.
  */
 void eliminar_barcos_jugador(barco_t barcos_jugador[CANT_BARCOS], int tope_barcos) {
     int i = 0;
@@ -238,8 +243,8 @@ void eliminar_barcos_jugador(barco_t barcos_jugador[CANT_BARCOS], int tope_barco
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: El archivo archivo_barcos debe estar abierto correctamente en modo lectura.
+ * POST: Crea todos los barcos del jugador, validando los barcos y el archivo. Devuelve el código de error en caso de un error, sino devuelve OK.
  */
 int jugador_crear (FILE* archivo_barcos, barco_t barcos_jugador[CANT_BARCOS]) {
     coordenada_t coordenada = {0, 0}; 
@@ -326,8 +331,8 @@ int jugador_crear (FILE* archivo_barcos, barco_t barcos_jugador[CANT_BARCOS]) {
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: Los barcos del jugador debe estar creada previamente con jugador_crear().
+ * POST: Crea la flota de barcos del jugador con los barcos creados previamente, y en cada coordenada de cada barco se pone false como impacto.
  */
 void inicializar_estado_flota(estado_barco_t flota_jugador[CANT_BARCOS], barco_t barcos_jugador[CANT_BARCOS]) {
     int i = 0;
@@ -347,8 +352,8 @@ void inicializar_estado_flota(estado_barco_t flota_jugador[CANT_BARCOS], barco_t
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: -
+ * POST: Inicializa el valor de cada fila y columna del tablero con un VACIO.
  */
 void inicializar_tablero(char tablero[FILA_MAX_TABLERO + 1][COLUMNA_MAX_TABLERO + 1]) {
     int fila = FILA_MIN_TABLERO;
@@ -368,8 +373,8 @@ void inicializar_tablero(char tablero[FILA_MAX_TABLERO + 1][COLUMNA_MAX_TABLERO 
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: Los barcos deben estar previamente creados con jugador_crear().
+ * POST: Carga los barcos en el tablero, cargando en cada fila y columna del tablero, un BARCO en cada coordenada correspondiente.
  */
 void cargar_barcos_en_tablero(char tablero[FILA_MAX_TABLERO + 1][COLUMNA_MAX_TABLERO + 1], barco_t barcos_jugador[]) {
     int i = 0;
@@ -396,30 +401,66 @@ void cargar_barcos_en_tablero(char tablero[FILA_MAX_TABLERO + 1][COLUMNA_MAX_TAB
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: -
+ * POST: Imprime por pantalla el emoji según el tipo de caracter recibido.
+ */
+void imprimir_emoticon(char caracter) {
+
+    switch (caracter) {
+
+        case BARCO:
+            printf("%s", EMOJI_BARCO);
+            break;
+
+        case AGUA:
+            printf("%s", EMOJI_AGUA);
+            break;
+
+        case TOCADO:
+            printf("%s", EMOJI_TOCADO);
+            break;
+
+        case HUNDIDO:
+            printf("%s", EMOJI_HUNDIDO);
+            break;
+
+        case VACIO:
+            printf("%s", EMOJI_VACIO);
+            break;
+    }
+}
+
+/*
+ * PRE: -
+ * POST: Muestra los tableros de jugador y de oponente por pantalla.
  */
 void mostrar_tableros(char tablero_jugador[FILA_MAX_TABLERO + 1][COLUMNA_MAX_TABLERO + 1], char tablero_oponente[FILA_MAX_TABLERO + 1][COLUMNA_MAX_TABLERO + 1]) {
     int fila = FILA_MIN_TABLERO;
     int columna = COLUMNA_MIN_TABLERO;
 
-    printf("\nTABLERO JUGADOR\t\tTABLERO OPONENTE\n\n");
+    printf("  ===================================================================\n");
+    printf("\t\t\t\tBATALLA NAVAL\n");
+    printf("  ===================================================================\n");
+
+    printf("\n\t  TABLERO JUGADOR\t\t  TABLERO OPONENTE\n\n");
 
     while (fila <= FILA_MAX_TABLERO) {
 
         columna = COLUMNA_MIN_TABLERO;
+        
+        printf("\t");
 
         while (columna <= COLUMNA_MAX_TABLERO) {
-            printf("%c ", tablero_jugador[fila][columna]);
+            imprimir_emoticon(tablero_jugador[fila][columna]);
             columna++;
         }
 
-        printf("\t");
+        printf("\t\t");
 
         columna = COLUMNA_MIN_TABLERO;
 
         while (columna <= COLUMNA_MAX_TABLERO) {
-            printf("%c ", tablero_oponente[fila][columna]);
+            imprimir_emoticon(tablero_oponente[fila][columna]);
             columna++;
         }
 
@@ -431,11 +472,10 @@ void mostrar_tableros(char tablero_jugador[FILA_MAX_TABLERO + 1][COLUMNA_MAX_TAB
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: -
+ * POST: Imprime por pantalla un mensaje solicitando el disparo al jugador y lee el disparo en FORMATO_DISPARO_JUGADOR, hasta que la coordenada sea válida. Devuelve el disparo.
  */
-coordenada_t pedir_disparo_jugador()
-{
+coordenada_t pedir_disparo_jugador() {
     coordenada_t disparo;
 
     do {
@@ -450,16 +490,16 @@ coordenada_t pedir_disparo_jugador()
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: El tablero debe estar previamente creado y cargado, el disparo del jugador debe ser válido.
+ * POST: Actualiza el tablero del oponente, colocando en la coordenada del disparo del jugador, el resultado del disparo.
  */
 void actualizar_tablero_enemigo(char tablero_oponente[FILA_MAX_TABLERO + 1][COLUMNA_MAX_TABLERO + 1], coordenada_t disparo_jugador, char resultado_del_disparo_jugador) {
     tablero_oponente[disparo_jugador.fila][disparo_jugador.columna] = resultado_del_disparo_jugador;
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: El disparo del enemigo debe ser válido. La flota de los barcos debe ser creada previamente y debe ser válida.
+ * POST: Actualiza el tablero del jugador, en función del disparo enemigo. Coloca AGUA si falla el disparo, o TOCADO si el disparo acierta a un barco.
  */
 void procesar_disparo_enemigo(coordenada_t disparo_enemigo, estado_barco_t flota_jugador[CANT_BARCOS], char tablero_jugador[FILA_MAX_TABLERO + 1][COLUMNA_MAX_TABLERO + 1]) {
     bool impacto = false;
@@ -491,8 +531,8 @@ void procesar_disparo_enemigo(coordenada_t disparo_enemigo, estado_barco_t flota
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: El barco debe estar previamente creado, e inicializado el estado del impacto.
+ * POST: Devuelve true si un barco fue hundido, es decir si en todas las coordenadas del barco, hubo impacto. False en caso contrario.
  */
 bool es_barco_hundido(estado_barco_t barco) {
     int i = 0;
@@ -511,8 +551,8 @@ bool es_barco_hundido(estado_barco_t barco) {
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: Los barcos deben estar creados previamente, e inicializados todos los estados de impacto.
+ * POST: Devuelve true si todos los barcos fueron hundidos, es decir si en todas las coordenadas de todos los barcos hubo impacto. False en caso contrario.
  */
 bool es_flota_jugador_hundida(estado_barco_t flota_jugador[CANT_BARCOS]) {
     int i = 0;
@@ -531,8 +571,8 @@ bool es_flota_jugador_hundida(estado_barco_t flota_jugador[CANT_BARCOS]) {
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: El archivo archivo_reporte debe estar abierto previamente en modo escritura.
+ * POST: Escribe en el archivo las estadísticas del juego: balas aliadas y enemigas acertadas, balas aliadas y enemigas erradas, barcos enemigos hundidos, barcos aliados sobrevivientes.
  */
 void generar_reporte(FILE* archivo_reporte, 
                     int balas_aliadas_acertadas, 
@@ -558,8 +598,8 @@ void generar_reporte(FILE* archivo_reporte,
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: -
+ * POST: Imprime por pantalla un mensaje de error en función del código de estado recibido. En caso de que no haya un error no se imprime nada.
  */
 void mostrar_error_jugador_crear(int estado_jugador_crear) {
     
@@ -599,8 +639,8 @@ void mostrar_error_jugador_crear(int estado_jugador_crear) {
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: -
+ * POST: Actualiza la cantidad de balas erradas, balas acertadas y de barcos hundidos, del jugador en función del resultado de su disparo.
  */
 void actualizar_reporte_por_disparo_jugador(char resultado_disparo, int *balas_acertadas, int *balas_erradas, int *barcos_hundidos) {
     if (resultado_disparo == AGUA) {
@@ -615,8 +655,8 @@ void actualizar_reporte_por_disparo_jugador(char resultado_disparo, int *balas_a
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: El oponente, la flota de barcos del jugador y el tablero del jugador deben estar previamente creados.
+ * POST: Obtiene disparos del enemigo hasta que la coordenada generada sea válida. Actualiza el estado del tablero del jugador y las estadísticas de balas enemigas según el resultado del disparo.
  */
 bool jugar_turno_enemigo(oponente_t *oponente, 
                          estado_barco_t flota_jugador[CANT_BARCOS], 
@@ -630,8 +670,6 @@ bool jugar_turno_enemigo(oponente_t *oponente,
         disparo_enemigo = oponente_realiza_disparo(oponente);
     } while (!es_coordenada_valida_tablero(disparo_enemigo));
 
-    printf("Disparo enemigo: %d;%d\n", disparo_enemigo.fila + 1, disparo_enemigo.columna + 1);
-
     procesar_disparo_enemigo(disparo_enemigo, flota_jugador, tablero_jugador);
 
     if (tablero_jugador[disparo_enemigo.fila][disparo_enemigo.columna] == AGUA) {
@@ -644,8 +682,8 @@ bool jugar_turno_enemigo(oponente_t *oponente,
 }
 
 /*
- * PRE: 
- * POST: 
+ * PRE: La flota del jugador debe estar previamente creada, con sus estados de impacto de cada barco.
+ * POST: Devuelve la cantidad de barcos que no fueron hundidos, es decir los barcos que no tienen en todas sus coordenadas un impacto.
  */
 int contar_barcos_aliados_sobrevivientes(estado_barco_t flota_jugador[CANT_BARCOS]) {
     int i = 0;
@@ -725,7 +763,7 @@ int main(int argc, char* argv[]) {
 
     while (!juego_terminado) {
 
-        // system("clear");
+        system("clear");
 
         mostrar_tableros(tablero_jugador, tablero_oponente);
 
